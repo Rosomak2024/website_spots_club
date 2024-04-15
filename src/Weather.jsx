6fc0db0d6e0973b43d0b6438a026f71857cc10a2
Rosomak2024 <mbnet@o2.pl>
2024-04-15 16:33:42 +0200

    chnages added

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,43 +1,42 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function WeatherComponent() {
-    const [weatherData, setWeatherData] = useState(null);
-  
-    useEffect(() => {
-      const apiUrl = 'https://api.open-meteo.com/v1/forecast';
-  
-      axios
-        .get(apiUrl, {
-          params: {
-            latitude: 52.2297,
-            longitude: 21.0122,
-            current_weather: true,
-          },
-        })
-        .then((response) => {
-          setWeatherData(response.data);
-        })
-        .catch((error) => {
-          console.error('Błąd pobierania danych pogodowych:', error);
-        });
-    }, []);
-  
-    return (
-      <div>
-        {weatherData ? (
-          <div className='weather_text'>
-            <h2>Pogoda w okolicy Dzika:</h2>
-            <p>Temperatura: {weatherData.current_weather.temperature}°C</p>
-            <p>Opady: {weatherData.current_weather.precipitation} mm</p>
-            <p>Wiatr: {weatherData.current_weather.windspeed} km/h</p>
+  const [weatherData, setWeatherData] = useState(null);
 
-          </div>
-        ) : (
-          <p>Ładowanie danych pogodowych...</p>
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    const apiUrl = "https://api.open-meteo.com/v1/forecast";
 
-  export  default WeatherComponent;
+    axios
+      .get(apiUrl, {
+        params: {
+          latitude: 52.2297,
+          longitude: 21.0122,
+          current_weather: true,
+        },
+      })
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Błąd pobierania danych pogodowych:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {weatherData ? (
+        <div className="weather_text">
+          <h2>Pogoda w okolicy Dzika:</h2>
+          <p>Temperatura: {weatherData.current_weather.temperature}°C</p>
+          <p>Opady: {weatherData.current_weather.precipitation} mm</p>
+          <p>Wiatr: {weatherData.current_weather.windspeed} km/h</p>
+        </div>
+      ) : (
+        <p>Ładowanie danych pogodowych...</p>
+      )}
+    </div>
+  );
+}
+
+export default WeatherComponent;
