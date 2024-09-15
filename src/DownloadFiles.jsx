15401ff08aecb2626fa49dac4cae08ce2353c5a4
Rosomak2024 <mbnet@o2.pl>
2024-09-15 15:45:58 +0200

    change path of public files

--- src/DownloadFiles.jsx
+++ src/DownloadFiles.jsx
@@ -5,7 +5,7 @@ const DownloadPdfFile = () => {
     <div>
       <a
         className="download_link"
-        href="../public/Umowa_zajęcia_sportowe_1raz.pdf"
+        href="../Umowa_zajęcia_sportowe_1raz.pdf"
         download="Umowa_zajęcia_sportowe_1raz.pdf"
       >
         Pobierz deklaracja na zajęcia raz w tygodniu PDF
@@ -13,7 +13,7 @@ const DownloadPdfFile = () => {
       <br></br>
       <a
         className="download_link"
-        href="../public/Umowa_zajęcia_sportowe_2razy.pdf"
+        href="../Umowa_zajęcia_sportowe_2razy.pdf"
         download="Umowa_zajęcia_sportowe_2razy.pdf"
       >
         Pobierz deklaracja na zajęcia dwa razy w tygodniu PDF
@@ -21,7 +21,7 @@ const DownloadPdfFile = () => {
       <br></br>
       <a
         className="download_link"
-        href="../public/oświadczenie_kontakt.pdf"
+        href="../oświadczenie_kontakt.pdf"
         download="oświadczenie_kontakt.pdf"
       >
         Pobierz Załącznik 1 PDF
@@ -29,7 +29,7 @@ const DownloadPdfFile = () => {
       <br></br>
       <a
         className="download_link"
-        href="../public/oświadczenie_rodo.pdf"
+        href="../oświadczenie_rodo.pdf"
         download="oświadczenie_rodo.pdf"
       >
         Pobierz Załącznik 2 PDF
@@ -37,8 +37,8 @@ const DownloadPdfFile = () => {
       <br></br>
       <a
         className="download_link"
-        href="../public/standardy_ochrony_małoletnich_CIR.pdf"
-        download="public/standardy_ochrony_małoletnich_CIR.pdf"
+        href="../standardy_ochrony_małoletnich_CIR.pdf"
+        download="standardy_ochrony_małoletnich_CIR.pdf"
       >
         Pobierz Standardy Ochrony Małoletnich PDF
       </a>
